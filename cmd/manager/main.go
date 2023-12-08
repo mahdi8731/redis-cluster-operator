98b3c7f35cab2c76cfed049e876e78896779858e
mahdi aghasi <mahdi.aghasi@outlook.com>
2023-12-08 23:14:39 +0330

    fix: fix probeAddr url

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -79,12 +79,12 @@ func printVersion() {
 func main() {
 	var metricsAddr string = "0.0.0.0:8383"
 	var enableLeaderElection bool = true
-	var probeAddr string = "0.0.0.:8585"
-	// flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	// flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	// flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-	// 	"Enable leader election for controller manager. "+
-	// 		"Enabling this will ensure there is only one active controller manager.")
+	var probeAddr string = "0.0.0.0:8585"
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
 		Development: false,
 	}
